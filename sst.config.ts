98c7a368ad8c384e9c793f7c16202d78123735fb
Aidan Lowson <dev@aidanlowson.com>
2024-08-04 21:53:46 +0100

    Adding custom domain

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import { domainName } from "@/app/constants/constants";
 
 export default {
   config(_input) {
@@ -12,7 +13,30 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const hostedZone = route53.HostedZone.fromHostedZoneAttributes(
+        stack,
+        "HostedZone",
+        {
+          hostedZoneId: process.env.HOSTED_ZONE_ID!,
+          zoneName: domainName,
+        }
+      );
+
+      // Create an ACM certificate for the domain
+      const certificate = new acm.Certificate(stack, "SiteCertificate", {
+        domainName,
+        validation: acm.CertificateValidation.fromDns(hostedZone),
+      });
+
+      const site = new NextjsSite(stack, "site", {
+        customDomain: {
+          domainName,
+          hostedZone: domainName,
+          cdk: {
+            certificate,
+          },
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
