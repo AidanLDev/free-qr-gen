3b83ebefab6dc00abf4b9229c96ba184808b4cc0
Aidan Lowson <dev@aidanlowson.com>
2024-08-05 22:53:15 +0100

    Adding GA tag

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Providerse({ children }: { children: ReactNode }) {
   return (
     <>
       <ToastContainer />
+      <GoogleAnalytics gaId="G-6Q96JQ2FS6" />
       {children}
     </>
   )
