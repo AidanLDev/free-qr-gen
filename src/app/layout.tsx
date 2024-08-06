019c165628a98b11c4ca64a0237b957051a982dc
Aidan Lowson <aidan.lowson@processvision.com>
2024-08-06 12:44:28 +0100

    Add stie maps and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,38 @@ export const metadata: Metadata = {
   title: 'Free QR Code Generator',
   description:
     'Customise and generate a QR code for free! Download and share your links in a convenient fun way.',
+  keywords: [
+    'QR code generator',
+    'Custom QR codes',
+    'QR code creator',
+    'Personalized QR codes',
+    'QR code design',
+    'QR code maker',
+    'QR code customization',
+    'Dynamic QR codes',
+    'Branded QR codes',
+    'QR code scanner',
+    'QR code marketing',
+    'QR code solutions',
+    'Colorful QR codes',
+    'Logo QR codes',
+    'QR code analytics',
+  ],
+  openGraph: {
+    title: 'Free QR Code Generator',
+    description:
+      'Customise and generate a QR code for free! Download and share your links in a convenient fun way.',
+    type: 'website',
+    siteName: 'freeqrgen.net',
+    images: [
+      {
+        url: 'https://freeqrgen.net/FreeQRGenCoverImage.png',
+        width: 1200,
+        height: 630,
+        alt: 'Free QR Code Generator Cover Image',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
