435d7faacf05c4f39bdbece05267d318f76daed7
AidanLDev <dev@aidanlowson.com>
2024-08-07 21:37:37 +0100

    Updating domain to add subdomain www.

--- sst.config.ts
+++ sst.config.ts
@@ -1,36 +1,38 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
-import * as route53 from "aws-cdk-lib/aws-route53";
-import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import { domainName } from "@/app/constants/constants";
+import { SSTConfig } from 'sst'
+import { NextjsSite } from 'sst/constructs'
+import * as route53 from 'aws-cdk-lib/aws-route53'
+import * as acm from 'aws-cdk-lib/aws-certificatemanager'
+import { domainName } from '@/app/constants/constants'
 
 export default {
   config(_input) {
     return {
-      name: "free-qr-gen",
-      region: "us-east-1",
-    };
+      name: 'free-qr-gen',
+      region: 'us-east-1',
+    }
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
       const hostedZone = route53.HostedZone.fromHostedZoneAttributes(
         stack,
-        "HostedZone",
+        'HostedZone',
         {
           hostedZoneId: process.env.HOSTED_ZONE_ID!,
           zoneName: domainName,
         }
-      );
+      )
 
       // Create an ACM certificate for the domain
-      const certificate = new acm.Certificate(stack, "SiteCertificate", {
+      const certificate = new acm.Certificate(stack, 'SiteCertificate', {
         domainName,
+        subjectAlternativeNames: [`www.${domainName}`],
         validation: acm.CertificateValidation.fromDns(hostedZone),
-      });
+      })
 
-      const site = new NextjsSite(stack, "site", {
+      const site = new NextjsSite(stack, 'site', {
         customDomain: {
           domainName,
+          domainAlias: `www.${domainName}`,
           hostedZone: domainName,
           cdk: {
             certificate,
@@ -38,13 +40,13 @@ export default {
         },
         environment: {
           HOSTED_ZONE_ID: process.env.HOSTED_ZONE_ID!,
-          BASE_URL: "https://freeqrgen.net",
+          BASE_URL: 'https://freeqrgen.net',
         },
-      });
+      })
 
       stack.addOutputs({
         SiteUrl: site.url,
-      });
-    });
+      })
+    })
   },
-} satisfies SSTConfig;
+} satisfies SSTConfig
