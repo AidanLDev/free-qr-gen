3fd4bf9ea712876d158863e171fc8351ffc1548b
Aidan Lowson <dev@aidanlowson.com>
2024-08-15 07:16:02 +0100

    Link styles and icons

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react'
 import FeedbackForm from './form/FeedbackForm'
 import BuyMeACoffeeButton from './BuyMeACoffeeButton/BuyMeACoffeeButton'
+import Link from 'next/link'
+import { FaLongArrowAltRight, FaBug } from 'react-icons/fa'
 
 export default function Footer() {
   const [formOpen, setFormOpen] = useState(false)
@@ -10,14 +12,23 @@ export default function Footer() {
     <footer className="w-full">
       <div className="flex w-full justify-between items-center">
         {!formOpen && (
-          <span
+          <div
+            className="flex gap-1 items-center"
             onClick={() => setFormOpen((prevState) => !prevState)}
-            className="font-bold text-center cursor-pointer hover:underline"
           >
-            Feedback/Report a bug
-          </span>
+            <span className="link-style">Feedback/Report a bug</span>
+            <FaBug className="text-primary" />
+          </div>
         )}
         {!formOpen && <BuyMeACoffeeButton />}
+        {!formOpen && (
+          <Link href="/support-me" className="self-baseline">
+            <div className="flex gap-1 items-center">
+              <span className="link-style">Support Me</span>
+              <FaLongArrowAltRight className="text-primary" />
+            </div>
+          </Link>
+        )}
       </div>
       <FeedbackForm
         feedbackFormOpen={formOpen}
