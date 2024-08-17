590cc1adbd9cec3397f15b0b2ea18ef521d27f19
AidanLDev <dev@aidanlowson.com>
2024-08-17 09:30:02 +0100

    Header messaeg update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { format, differenceInDays } from 'date-fns'
 import QRInfoForm from './components/form/QRInfoForm'
 import { Metadata } from 'next'
 import JsonLd from './components/JsonLd'
@@ -9,8 +8,6 @@ export const metadata: Metadata = {
   },
 }
 
-const siteCreatedDate = new Date('2024/08/04')
-
 export default function Home() {
   return (
     <div className="min-h-screen  px-8 sm:px-24 flex flex-col justify-between pb-4">
@@ -18,25 +15,11 @@ export default function Home() {
         <JsonLd />
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-2xl">
           <h1 className="fixed left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:bg-zinc-800/30 text-primary">
-            Generate a free QR Code
+            Welcome! Generate a free QR Code
           </h1>
           <h2 className="text-center text-xl sm:my-4 mb-4 mt-28">
-            This site is under construction, it was created on{' '}
-            <span className="italic text-secondary">{`${format(
-              siteCreatedDate,
-              'yyyy'
-            )} the ${format(siteCreatedDate, 'do')} of ${format(
-              siteCreatedDate,
-              'MMMM'
-            )}`}</span>
-            ,{' '}
-            <span className="text-secondary">
-              {differenceInDays(new Date(), siteCreatedDate)}
-            </span>{' '}
-            days ago...
-          </h2>
-          <h2 className="text-center text-xl my-4">
-            So watch this space and expect a lot of development
+            Just enter the destination you want your QR Code to link to and you
+            can configure how your QR code looks in the form below
           </h2>
           <QRInfoForm />
         </div>
