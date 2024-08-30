40cd9e251e0eafaa64d899180675006078906285
AidanLDev <dev@aidanlowson.com>
2024-08-30 22:56:29 +0100

    Updating packages and fixing url

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,6 @@
 export const domainName = 'freeqrgen.net'
-export const urlRegex = /^[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/
+export const urlRegex =
+  /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?(\S*)?$/
 export const postHeader = {
   'Content-Type': 'application/json',
 }
