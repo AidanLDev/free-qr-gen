fa756f04c1f0dd4a8ea3bb2fc5f7dd10ea496910
Aidan Lowson <dev@aidanlowson.com>
2024-09-26 21:28:53 +0100

    Feedback styles

--- src/app/components/form/FeedbackForm.tsx
+++ src/app/components/form/FeedbackForm.tsx
@@ -33,42 +33,41 @@ export default function FeedbackForm({
   }
   return (
     <div
-      className={`border-2 border-primary rounded-md max-w-60 p-4 overflow-hidden feedback-form-wrapper sm:mb-0 mb-12 ${
+      className={`border-2 border-slate-700 rounded-md max-w-80 p-4 overflow-hidden feedback-form-wrapper sm:mb-0 mb-12 text-xl ${
         feedbackFormOpen ? 'open' : ''
       }`}
     >
-      <div className="flex justify-between">
-        <h1 className="text-lg font-bold text-center text-primary">
+      <div className="flex justify-center">
+        <h1 className="text-lg font-bold text-center text-primary w-full">
           Get in touch!
         </h1>
         <MdClose
-          className="cursor-pointer w-[24px] h-[24px] text-secondary"
+          className="cursor-pointer w-[24px] h-[24px] text-white"
           onClick={() => {
             setFeedback('')
             setFeedbackFormOpen(false)
           }}
-          width={24}
-          height={24}
+          width={28}
+          height={28}
         />
       </div>
-
-      <p className="py-2">
-        Spotted an issue? Let me know and I will fix it! 😊
-      </p>
-      <div className="flex flex-col gap-4 text-center">
-        <TextArea
-          value={feedback}
-          setValue={setFeedback}
-          id="feedback"
-          label="Feedback:"
-        />
-        <Button
-          label="Submit Feedback"
-          className="max-w-40 self-center"
-          onClick={async () => await handleSubmitFeedback()}
-        />
+      <div className='flex flex-col gap-4 text-center'>
+        <p className="py-2">Spotted an issue? Let me know and I will fix it!</p>
+        <div className="flex flex-col gap-4 text-center">
+          <TextArea
+            value={feedback}
+            setValue={setFeedback}
+            id="feedback"
+            label="Feedback:"
+          />
+          <Button
+            label="Submit Feedback"
+            className="max-w-[75%] self-center"
+            onClick={async () => await handleSubmitFeedback()}
+          />
+        </div>
+        <p className="text-center py-2">I am a human, please do be kind 💖</p>
       </div>
-      <p className="text-center py-2">I am a human, please do be kind 💖</p>
     </div>
   )
 }
