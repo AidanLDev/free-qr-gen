27bf648bb9bc011b47bab3e694656c168dc7ad1f
Aidan Lowson <dev@aidanlowson.com>
2025-11-20 16:15:43 +0000

    Fixin build issue with sentry

--- src/app/api/sentry-example-api/route.ts
+++ src/app/api/sentry-example-api/route.ts
@@ -1,6 +1,5 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const dynamic = 'force-dynamic'
 class SentryExampleAPIError extends Error {
   constructor(message: string | undefined) {
     super(message)
@@ -8,7 +7,9 @@ class SentryExampleAPIError extends Error {
   }
 }
 // A faulty API route to test Sentry's error monitoring
-export function GET() {
+export function GET(request: NextRequest) {
+  // Access request to make this route dynamic without using 'dynamic' export
+  const headers = request.headers
   throw new SentryExampleAPIError('This error is raised on the backend called by the example page.')
   return NextResponse.json({ data: 'Testing Sentry Error...' })
 }
